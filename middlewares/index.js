66a529fdf1d7bdebaa892aee535835d6d96c37a6
mernxl <mernxl@gmail.com>
2020-05-09 17:23:45 +0100

    feat: add api error classes, implement catch all handlers in middlewares

--- middlewares/index.js
+++ middlewares/index.js
@@ -13,13 +13,23 @@ const runMiddlewares = async (_middlewares, count, reqRes) => {
   return _middlewares[count](await runMiddlewares(_middlewares, count - 1, reqRes));
 };
 
-const Middleware = (...rest) => (req, res) => {
+const Middleware = (...rest) => async (req, res) => {
   // loop through middlewares
   const functions = middlewares.concat(rest);
   for (let i = 0; i < functions.length; i += 1) {
     if (typeof functions[i] !== 'function') throw new Error(`${functions[i].toString()} is not a function`);
   }
-  return runMiddlewares(functions, functions.length - 1, { req, res });
+
+  try {
+    await runMiddlewares(functions, functions.length - 1, { req, res });
+  } catch (e) {
+    res.statusCode = e.status || 500;
+    return res.end(JSON.stringify(e));
+  }
+
+
+  res.statusCode = 404;
+  return res.end('Resource Not Found');
 };
 
 export default Middleware;
