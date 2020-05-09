6c7db9fee31705fa421dab7a2a1c4e26e88acda3
mernxl <mernxl@gmail.com>
2020-05-09 22:47:52 +0100

    feat(libs): add `setUserSession` function to auth-helpers, fix config constant init

--- libs/auth-helpers.js
+++ libs/auth-helpers.js
@@ -7,12 +7,12 @@ const properties = PropertiesReader(path.resolve('voxnostra.properties'));
 
 export const SALT_WORK_FACTOR = Number(properties.get('auth.SALT_WORK_FACTOR') || 10);
 
-export const PWD_MAX_LOGIN_ATTEMPTS = Number(properties.get('PWD_MAX_LOGIN_ATTEMPTS') || 10);
-export const PWD_LOCK_TIME = moment.duration({ minute: Number(properties.get('PWD_LOCK_TIME') || 5) });
+export const PWD_MAX_LOGIN_ATTEMPTS = Number(properties.get('auth.PWD_MAX_LOGIN_ATTEMPTS') || 10);
+export const PWD_LOCK_TIME = moment.duration({ minute: Number(properties.get('auth.PWD_LOCK_TIME') || 5) });
 
-export const PWD_RESET_TOKEN_EXP = moment.duration({ hours: Number(properties.get('PWD_RESET_TOKEN_EXP') || 5) });
-export const JWT_SECRET = properties.get('JWT_SECRET') || 'XYZ';
-export const JWT_ISSUER = properties.get('JWT_ISSUER') || 5;
+export const PWD_RESET_TOKEN_EXP = moment.duration({ hours: Number(properties.get('auth.PWD_RESET_TOKEN_EXP') || 5) });
+export const JWT_SECRET = properties.get('auth.JWT_SECRET') || 'XYZ';
+export const JWT_ISSUER = properties.get('auth.JWT_ISSUER') || 'xyz';
 
 export function verifyResetToken(
   token
@@ -24,4 +24,14 @@ export function verifyResetToken(
 
     return resolve({ payload, valid: true });
   }));
+}
+
+export function setUserSession(req, user) {
+  if (user) {
+    req.session.user = {
+      id: user.id,
+      email: user.email,
+      name: user.name
+    };
+  }
 }
