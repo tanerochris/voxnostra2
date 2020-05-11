6cba8cb13cba58d76e1caa899bca85cbebc11535
mernxl <mernxl@gmail.com>
2020-05-11 10:00:02 +0100

    7-feat(schema): add email validation regex with onFail message

--- schemas/user/user.model.js
+++ schemas/user/user.model.js
@@ -5,7 +5,11 @@ import UserStatics from './user.statics';
 const UserSchema = new Schema({
   name: String,
   email: {
-    type: String, index: true, unique: true, required: true
+    type: String,
+    index: true,
+    unique: true,
+    required: true,
+    validate: [/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'Must be a valid email address.']
   },
 
   password: { type: PasswordSchema, select: 0 }
