f09777dcf23d59b87e442fda85d273b53c928fdc
mernxl <mernxl@gmail.com>
2020-05-19 21:54:57 +0100

    4-feat(schema): add app schema based on api-docs and endpoints
    - add project schema
    - add user schema

--- pages/api/testapi.js
+++ pages/api/testapi.js
@@ -1,10 +1,10 @@
 import Middleware from '../../middlewares';
-import User from '../../schemas/user.model';
+import UserModel from '../../schemas/user/user.model';
 
 const TestHandler = async ({ res }) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
-  const user = new User({ name: 'leopoldo' });
+  const user = new UserModel({ name: 'leopoldo' });
   await user.save();
   res.end(JSON.stringify({ name: 'leopold' }));
 };
