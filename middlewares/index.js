9f2eda482f57cf108914031c970bc7af62bd1573
tanerochris <tanejuth@gmail.com>
2020-05-20 07:03:14 -0700

    14-refactor(structure): Rename folders and moved files
    - documented project api endpoints
    - refactored project structure, renamed folders and moved files raised in #16
    - added prettier configuration
    
    partially resolve #14 reviews

--- middlewares/index.js
+++ middlewares/index.js
@@ -5,7 +5,8 @@ const cors = ({ req, res }) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-  return { req, res };
+  const httpReqRes = { req, res };
+  return httpReqRes;
 };
 
 /**
