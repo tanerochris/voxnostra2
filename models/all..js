88d963b067956375d5b2188de79c56686d54fdac
tanerochris <tanejuth@gmail.com>
2020-06-03 14:58:20 -0700

    23-feat(application): Combining all PRs
    merged the different PRs
    
    closes #23

--- models/all..js
+++
@@ -1,5 +0,0 @@
-import User from './user/user.model';
-import Project from './project/project.model';
-import Media from './media/media.model';
-
-export default { User, Project, Media };
--- pages/api/health-check.js
+++
@@ -1,7 +0,0 @@
-import Middleware from '../../middlewares';
-
-const HealthCheckHandler = async ({ req, res }) => {
-  res.send(`All Good, Thanks for checking in - @${(req.session.user && req.session.user.name) || 'MrAnonymous'}.`);
-};
-
-export default Middleware(HealthCheckHandler);
