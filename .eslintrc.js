639f33a467490ebba0e9765eee714df9b8492545
mernxl <mernxl@gmail.com>
2020-06-07 16:59:42 +0100

    chore(build): add use babel-eslint as eslint parser
    This change enables features on babel like optional chaining, and removes eslint errors related to the features

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,5 @@
 module.exports = {
+  parser: 'babel-eslint',
   env: {
     browser: true,
     es6: true,
@@ -15,16 +16,17 @@ module.exports = {
     sourceType: 'module'
   },
   rules: {
+    strict: 0,
     'prettier/prettier': ['error'],
     'comma-dangle': ['error', 'never'],
     'no-param-reassign': ['error', { props: false }],
     'react/react-in-jsx-scope': 'off',
-    'import/no-dynamic-require': 'off',
+    'import/no-dynamic-require': 'off'
   },
   plugins: ['react', 'prettier'],
   settings: {
     react: {
-      version: 'detect', 
+      version: 'detect'
     }
-  }   
+  }
 };
